7c813d6db792505f82dd72a50b0592ebf3557323
Jarek Dzwolak <jaroslawdzwolak@gmail.com>
2024-02-26 18:14:23 +0100

    add href

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => {
           className="flex items-center justify-between p-3 lg:px-8"
           aria-label="Global">
           <div className="flex lg:flex-1">
-            <a className="-m-1.5 p-1.5">
+            <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img className="w-14" src={Logo} alt="Company Logo" />
             </a>
@@ -61,7 +61,9 @@ const Header = () => {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a className="text-sm font-semibold leading-6 text-gray-900">
+            <a
+              href="/"
+              className="text-sm font-semibold leading-6 text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
@@ -74,7 +76,7 @@ const Header = () => {
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5 ">
+              <a href="/" className="-m-1.5 p-1.5 ">
                 <span className="sr-only">Your Company</span>
                 <img className="w-14" src={Logo} alt="Company Logo" />
               </a>
@@ -101,7 +103,7 @@ const Header = () => {
                 </div>
                 <div className="py-6">
                   <a
-                    href="#"
+                    href="/"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                     Log in
                   </a>
