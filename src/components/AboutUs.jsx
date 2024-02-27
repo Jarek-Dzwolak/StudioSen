21daac13200704788d070844ec0cb5efb3988ac0
Jarek Dzwolak <jaroslawdzwolak@gmail.com>
2024-02-27 15:42:33 +0100

    fix text

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -17,16 +17,16 @@ function AboutUs() {
           <div>
             <h2 className="text-3xl  font-bold mb-4">O nas</h2>
             <p className=" leading-loose mb-6">
-              Naszą główna misja jest zapewnienie satysfakcji każdej osobie
+              Naszą główną misją jest zapewnienie satysfakcji każdej osobie
               która zdecyduje się skorzystać z naszych usług. Szczycimy się
               indywidualnym podejściem do klienta. Rozumiemy, że każdy ma inne
               potrzeby i oczekiwania, dlatego dokładamy wszelkich starań, by
               sprostać oczekiwaniom każdego , kto przekracza nasz próg. Do
-              każdego projektu podchodzimy personalnie – wasze zadowolenie z
+              każdego projektu podchodzimy personalnie – Wasze zadowolenie z
               wykonanej pracy to nasz priorytet.
             </p>
             <p className=" leading-loose">
-              Zapewniamy komfort i profesjonalna usługę na najwyższym poziomie.
+              Zapewniamy komfort i profesjonalną usługę na najwyższym poziomie.
               Dokładamy wszelkich starań żeby wasza wizyta była przyjemna a
               dzieło sztuki zostawione na wasze skórze cieszyło każdego dnia.
               Wszystkie wykonane przez nas projekty są niepowtarzalne.
