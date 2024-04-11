89d3d39fe19846d28c50c7342b7906968e17abec
Jarek Dzwolak <jaroslawdzwolak@gmail.com>
2024-04-11 17:06:11 +0200

    liter fixed

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -28,10 +28,10 @@ function Team() {
       <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
         <div className="max-w-2xl">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Poznaj naszą ekipe
+            Poznaj naszą ekipę
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Jesteśmy zgranym zespołem, gotowym sprostać każdym wyzwanią. Nasza
+            Jesteśmy zgranym zespołem, gotowym sprostać każdym wyzwaniom. Nasza
             pasja i zaangażowanie to nasza siła, która napędza nas do działania.
           </p>
         </div>
